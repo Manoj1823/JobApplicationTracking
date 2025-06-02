55c4ed9da532d2b95c768fab42a020832763ba9a
ManojKiran_CS <102407720+Manoj1823@users.noreply.github.com>
2025-06-02 15:59:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
-    outDir: path.resolve(__dirname, 'dist'),  // output at project root level, alongside backend folder
+    outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
 });
